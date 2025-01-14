9079ecead1a9bfebd484ad92accfff6f02f15903
Mikephie <131840160+Mikephie@users.noreply.github.com>
2025-01-14 22:28:18 +0800

    new

--- src/plugin/part2ai.js
+++ src/plugin/part2ai.js
@@ -1,27 +1,3 @@
-function decoder(p, a, c, k, e, d) {
-    e = function(c) {
-        return (c < a ? '' : e(parseInt(c / a))) + ((c = c % a) > 35 ? String.fromCharCode(c + 29) : c.toString(36));
-    };
-    if (!''.replace(/^/, String)) {
-        while (c--) {
-            d[e(c)] = k[c] || e(c);
-        }
-        k = [function(e) {
-            return d[e];
-        }];
-        e = function() {
-            return '\\w+';
-        };
-        c = 1;
-    }
-    while (c--) {
-        if (k[c]) {
-            p = p.replace(new RegExp('\\b' + e(c) + '\\b', 'g'), k[c]);
-        }
-    }
-    return p;
-}
-
 function plugin(code) {
     try {
         let result = code;
@@ -30,7 +6,7 @@ function plugin(code) {
         while (result.includes('eval(function(p,a,c,k,e,d)')) {
             result = result.replace(/eval\((function\(p,a,c,k,e,d\)[\s\S]*?})\(([\s\S]*?)\)\)/, (match, func, args) => {
                 try {
-                    return decoder.apply(null, args.split(','));
+                    return eval('(' + func + ')(' + args + ')');
                 } catch (e) {
                     console.error('Decode error:', e);
                     return match;

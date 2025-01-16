1cfad0afcd1d634ceb538a564cdf50918cf347ae
action <action@github.com>
2025-01-16 02:59:11 +0000

    Add decoded output file

--- output.js
+++ output.js
@@ -1,4 +1,4 @@
-//Thu Jan 16 2025 02:50:46 GMT+0000 (Coordinated Universal Time)
+//Thu Jan 16 2025 02:59:11 GMT+0000 (Coordinated Universal Time)
 //Base:https://github.com/echo094/decode-js
 //Modify:https://github.com/smallfawn/decode_action
 const a = $request.url;

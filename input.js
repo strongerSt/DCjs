3d74db2d9c97edb460de89a4286d32ab5b7dabf2
Mikephie <131840160+Mikephie@users.noreply.github.com>
2025-03-21 17:18:48 +0800

    Update input.js

--- input.js
+++ input.js
@@ -1,36 +1,26 @@
-// 加密脚本 - 自动解密执行
-// 原始脚本已加密保护
+/*************************************
+项目名称：快搜搜题
+更新日期：2025-03-20
+脚本作者：@Sheepfj
+TG频道：https://t.me/sheep_007xiaoyang
+GitHub：https://github.com/SheepFJ/QuantumultX
+使用声明：⚠️仅供参考，🈲转载与售卖！
+脚本说明: 解锁会员
 
-// 解密并执行模块
-function run() {
-  // 密钥和加密内容
-  const key = "ScriptableKey88";
-  const encoded = "fEl48KWCrFTinYlCPgAvARBMbhozUuKdgXrwr4uY8KqrlfCrm7Lwq5uA8JyKgfCmu5rwrquQ8K6LjfCqm5vwqYuc8KC7nPCsmoHwnIuq8Kq7i/Com4nwr4uY8KqrlfCrm7Lwq5uA8JyKgfCmu5p4YysGBBUeDD8AJlRXMAIeNFBbTkI9ECoLDU1VJg8GSSh+PwoYETsWQ2QXD0wVGBxZBwcIOWUXHFxcOhcuRxMbDD5DRT4XFRhLMBEbGQRZEwcfFSQLCl0VMQwWEFAcFRYcFnFKVkpZJE0VAAQcFAAZFi4XGldWJwYcHV4XDg9DKCIOHEhQOgZdOhMGCBIYSiYEEFYXIhtdGxUQBQsYEyIVV1JLWWkpOhMGCBIYOGtKVhhrJhEVDHomBAYIDD8zMGgYbkMGEAARXAoYETtIC11LIwwcGhVYQRINET8AC1YFDQsGHQAHWz5DOWQCCFQVNQYWNV4GBAYIDD85V1tXPj9dRVAGBBMZDDkAChVaPAcLVAQGFAdARSYEARVLOhkXVEBYQREPFyIVDRVIMhcaVBgAFRIfX2RKC1lPfQQbHRgBAxcfADkGFlZMNg0GRxMbDE0hDCAACVBRNkwhCgIdERZDCCoMFxdJK0wADBQQCBYaDDtLE0sUcxcbBBUbFBZRU3tvc2NrMBEbGQQpQU1DRQcKFlYyOxcGGV0GBBEcCiUWHBhmOxcGGQNOPU0wSiwUFRVeNgcuRwIRBQYFERdLGldVD0xSGhMGCBIYSDsEDVAFOxcGGQNOTk0eBDxLHlFMOxYQHAMREwEDCz8AF0wWMAwfRj0dCgccDSIAVmtbIQoCHV8ZAAsCSjodVkpdNwcbHQYdEUwGFmdFC11JJgoADANZAw0IHHYRC01df0MGAB0RDhcYWH1VczJjHiomJC1+CQ0fESUEFF0YbkMVGBxZBwcISzkAHVxRJ00RBh1+a0hDb0ERC0EYKGlSSVBUTk1M6K+b57yl5bux55WR6YW1572W5L6y5oCMeElQVEEBAws4EVlZSCMtEwQVVFxCTuKdjRkAHVxRJzU7OeKdmFZaaExFa0UaV1YgF1IIBQAJDR5FdkVb8JGFpOKSi/CntKjikr3wo4Sr4pK28KektOKSsWlecxgYc0MRBh4HFUIBADgWGF9dc15SS+axiOS4seimgumVo+aJukXikrTinJhU4pOH4p2FfuKclOKSjeKclOKcjVZaaExFa0VzGBhzQ11GUOiml+afseWTruS+guaVnOWShuW7seS8qjIYc0NSBRUAQQADATJecxgYc0MGGwlUGmhMRWtFWRgYcwEdDQlUXEImNgQrV0hZIRAXQVQGBBEcCiUWHBZaPAcLRwIREQ4NBi5NVhpRIC0BDwdWWxYeEC5KHhQYdEEbGj4HBxVOXy0EFUtddEpbUnpUQUJMRWtFWTIYc0NSSVBUQQEDCzgRWUpdPgwEDDEQEkJRRWMKG1IRc15MSQt+QUJMRWtFWRgYc0NSABZUSUMDByFFBUQYJxoCDB8SQQ0OD2tERAUYdAwQAxUXFUVFRTkADU1KPUMdCxpPa0JMRWtFWRgYc0NSSRkSQUotFzkEABZRICIAGxENSQ0OD2JMWUMyc0NSSVBUQUJMRWtFWRgYcxEXHQUGD0IDByFLFFlIexEXBB8CBCMIFmJLH1FUJwYAQRkABA9MWHVFWBAyc0NSSVBUQUJMRWtFWRgYc0NSSVAdFQcBWmULFlxdbE0tNgQNEQcCBCYAWQUFbkNVKBQkDhEYQmsZBTIYc0NSSVBUQUJMRWtFWRgYc0NSSRkABA9TSyUKHV0HfQAXBRwHXkwfCiYAUVtdPw9SVE5UAgcACXRLJmdMKhMXBxEZBEJRWHZFXnlcHgYGCBQVFQMvACcJXhEYLx94SVBUQUJMRWtFWRgYc0NSSVBUQUIFES4IRhZWPAcXVl4VBTINHCcKGFwyc0NSSVBUQUJMRWtFWRgYc0pbUnpUQUJMRWtFWRgYc0MPY1BUQUJMRWtFWRgYc2lSSVBUQUJMRWtFWRhKNhcHGx5ULgAGACgRV15KPA43BwQGCAcfTUFFWRgYc0NSSVBUQUJMRWtFNlpSNgAGRxUaFRAFADhNFlpSek0fCABcSTkHADJJWU5ZPxYXNFlUXFxMPkFFWRgYc0NSSVBUQUJMRWtFWRgYcwgXEFx+QUJMRWtFWRgYc0NSSVBUQUJMRWsOHEEYbl5PSVcXDg8BACURCmhZNAYzDQNTQV1MPhZFQzIYc0NSSVBUQUJMRWtFWRgYc0NSSRsRGEJRWHZFXl1UNg4XBwQHRkJKQ2sTGFRNNlxcDBQTBBFMWmseWRYWfRUTBQURTUIJASwACgIYIQYfBgYRIAYfTT0EFU1dfQYWDhUHSEIRRXFvWRgYc0NSSVBUQUJMRWtFWRgYc0MADB0bFwctAThND1lUJgZbY1BUQUJMRWtFWRgYc0NSSVApSGhMRWtFWRgYc0NSSVBdWmhMRWtFWRgYcx5JY3pUQUJMRWtFWVpXNxpSVFA+Mi0iSzsEC0tdeykhJj5aEhYeDCUCEF5BexEXBB8CBCMIFmMHFlxBekp4SVBUQUJMRWtFWRgYfREXGRwVAgdESmkMCmhKNg4bHB05BA8OADlHQ15ZPxAXRhdYQUVODDg1C11VOhYfJBUZAwceR3ERC01ddEp4SVBUQUJMRWtFWRgYfREXGRwVAgdESmkMCmtNMRARGxkWBAZOXy0EFUtdfAReSVdWCBE/ECkWGkpRMQYWS0oAExcJQmJvWRgYc0NSSVBUQUJMSzkACVRZMAZaRlIdEicBFScKAF1dcVkUCBwHBE0LSWtCW1FLFg4CBR8NBAdOXz8XDF0femlSSVBUQUJMRWtFWRgWIQYCBREXBEpDRzgODEsaaUMuMiwpTgVARWxHClNNIEFISSsPQwkFCy9HQxpoIQYfAAUZQ05OFj4HCltKOhMGAB8aNRscAGlfW2hKNg4bHB1WTUACBCYAWwIaAxEXBBkBDEI/ECkWGkpRIxcbBh5WTUAIADgGC1FIJwodB1JOQy8DByIJHBh5PQ0HCBxUMRAJCCIQFBhrJgEBCgIdERYFCiVHVRpcJhETHRkbD0BWHmkEFFdNPRdQU0FYQxcCDD9HQxphFiIgSw1YQwsIR3FHSBoUcRIHCB4ACBYVR3FHSBoUcREXBxUDKAwYADkTGFQaaUErLDEmQ05OFy4UDFFKNgciCAkZBAwYNTkKD1FcNhEBS0ovQyM8NQcgJnF2EjMiS1xWJi0jIgcgJnF2EjMiSy1YQwcUES4XF1lUAxEdDQUXFSsIR3FHGldVfREXDRQdFUwcFy4IEE1VDFFQRVIEEw0BCjhHQ2Nlf0EGCBcHQ1g3ODY4XhEyc0NSSVBUQUJMRWtFV0pdIw8TChVcTkoXTGRJWR8cYkEaCAMrBg0AARQWDFpLMBEbGQQdDgxOXz8XDF0UcRMADBYrABcYCjsJGEEaaQUTBQMRTUAEBDg6Ck1aIAAAABIRBT0YChQVC11VOhYfS0oAExcJSWkNGEtnJQoBAAQRBT0CADw6CUpXNQoeDFJOFRAZAGdHCUpdNTwEABQRDj0NED8KCVRZKkFIDxEYEgdARy0AGExNIQYBS0oPQxIeCiYKDV1cDBcADB4QPgAABCUOChoCNQIeGhUJTUAFFhQIFlwaaRcAHBVYQxcfADk6EEtnIBYQGhMGCAAJF2lfDUpNNk9QARkQBD0NAThHQ0xKJgZeSxkHPgUDCS9HQ0xKJgZeSxkHIxANCy8kH15RPwoTHRVWWxYeEC5JW1BZIDwbBgMrEhcOFigXEEhMOgwcS0oAExcJSWkWHF1WDBMADB0dFA8zBC8HFVdbODwfBhQVDUBWETkQHBQaOwIBNhUMFQceCyoJJllbMAwHBwRWWxYeEC5JXhERaGlSSVBUHEIPBD8GERgQNkpSEnpUQUJMRWtFWRcXc+WnoeafruimiuafoOWlhei1hO+9ruS+seaNpOWPlOWmruWStOW6rOS9q1lDUklQVEFCTAckAQAYBXMpISY+WhEDHhYuTV1KXSATHQcDEU8AAwEyTEIyGHNDUhR6fkFCTEVkSlnpgKLnn53li4zogp54SVBUQQEDCzgRWVtXPA8WBgcaLAsCED8AChgFc1JCUnpUQUJMBiQLCkwYMAwdBRQbFgwhFmtYWVtXPA8WBgcaLAsCED8AChgSc1VCSVpUUFJcVXBvWRgYcwAdBwMAQQwDESIDAHNdKkNPSVIGBAYIDD86D1FIDA0dHRkSGD0HADI6DwkaaGlSSVBUAg0CFj9FF1dPc15SLREABEwCCjxNUAMyc0NSSRwRFUIABDgRN1dMOgULPRkZBEJRRXtecxgYc0N4SVBUQRYeHGsecxgYc0NSSVBUAg0CFj9FCkxXIQYWPRkZBEJRRW8VHEpLOhAGDB4AMhYDFy5LC11ZN0scBgQdBxsnADJMQjIYc0NSSVBUQQsKRWMWDVdKNgcmAB0RSEIXb2tFWRgYc0NSSVBUQQ4NFj8rFkxRNRomAB0RQV9MFSoXCl1xPRdaGgQbEwcIMSIIHBEDWUNSSVBUQUJMRWtFWVFec0sbGj4VL0oABDgRN1dMOgULPRkZBEtFRScECkx2PBcbDwkgCA8JRXZFSQMyc0NSSVBUQUIRb2tFWRhFcwATHRMcQUoJTGsecxgYc0NSSVBUDQMfEQUKDVFeKjcbBBVUXEJcXkFFWRgYLmlSSVBUa0JMRWsMHxgQPQwFSV1UDQMfEQUKDVFeKjcbBBVUX0IPCiQJHVdPPS4BQFAPa0JMRWtFWRgYOgVSQQQNEQcDA2tBF1dMOgUbChEACA0CRWpYRBgfJg0WDBYdDwcIQmJFAjIYc0NSSVBUQUJMRWtBF1dMOgUbChEACA0CSzsKCkwQMhMCJxEZBE5MBD4REVdKf0MfDAMHAAUJTHBvWRgYc0NSSVAJQQcAFi5FEF4YexcLGRUbB0JICyQREF5Bc0JPVFBTFAwIAC0MF11cdEpSEnpUQUJMRWtFWRgYc0NWBx8ACAQVTSoVCXZZPgZeSREBFQoDF2dFFF1LIAIVDFlPa0JMRWtFWRgYLmlSSVBUQUJMRW8VHEpLOhAGDB4AMhYDFy5LDkpRJwZaBx8DTxYDNj8XEFZfe0peSR4bFQsKHAAAABEDWUNSSVAJa2hMRWtFXVxXPQZaElAWDgYVX2svKnd2fRAGGxkaBgsKHGMHFlxBekMPQEt+HEIPBD8GERgQNkpSEnpUQUJMQS8KF10QKEMQBhQNW0JIFy4WCVdWIAZcCx8QGEIRTHBvBA==";
-  
-  // 解密函数
-  function decrypt(encoded, key) {
-    // 从Base64解码
-    const bData = Data.fromBase64String(encoded);
-    const text = bData.toRawString();
-    
-    // 异或解密
-    let result = "";
-    for (let i = 0; i < text.length; i++) {
-      const charCode = text.charCodeAt(i) ^ key.charCodeAt(i % key.length);
-      result += String.fromCharCode(charCode);
-    }
-    return result;
-  }
-  
-  // 解密并执行
-  try {
-    const script = decrypt(encoded, key);
-    eval(script);
-  } catch(e) {
-    console.error("执行错误: " + e);
-    if (typeof $done === 'function') $done({});
-  }
-}
+============ Quantumult X ============
 
-// 运行脚本
-run();
+[rewrite_local]
+^https://kspay\.iksdt\.com/pay\.php url script-response-body https://raw.githubusercontent.com/SheepFJ/QuantumultX/refs/heads/main/QuantumultX/APP/kuaisousouti/kuaisousouti.js  
+[mitm]
+hostname = kspay.iksdt.com
+
+*************************************/
+
+
+const _0x74af7f=_0x4d7a;(function(_0x1b4443,_0x255278){const _0x17bac3=_0x4d7a,_0x3320e7=_0x1b4443();while(!![]){try{const _0x598232=parseInt(_0x17bac3(0xf1))/0x1+parseInt(_0x17bac3(0x110))/0x2+-parseInt(_0x17bac3(0x107))/0x3+parseInt(_0x17bac3(0x104))/0x4+parseInt(_0x17bac3(0xf7))/0x5*(parseInt(_0x17bac3(0xff))/0x6)+-parseInt(_0x17bac3(0x105))/0x7*(parseInt(_0x17bac3(0xf0))/0x8)+-parseInt(_0x17bac3(0x10f))/0x9*(parseInt(_0x17bac3(0x109))/0xa);if(_0x598232===_0x255278)break;else _0x3320e7['push'](_0x3320e7['shift']());}catch(_0x15185c){_0x3320e7['push'](_0x3320e7['shift']());}}}(_0x3161,0x39010));let body=JSON['parse']($response[_0x74af7f(0x108)]);function _0x4d7a(_0x49acf7,_0x43234f){const _0x5a7299=_0x3161();return _0x4d7a=function(_0x4e51fa,_0x10eaa9){_0x4e51fa=_0x4e51fa-0xef;let _0x24d192=_0x5a7299[_0x4e51fa];return _0x24d192;},_0x4d7a(_0x49acf7,_0x43234f);}function modifyObject(_0x1018bc){const _0x58cddf=_0x74af7f,_0xb67afe=(function(){let _0x500ba0=!![];return function(_0x28d857,_0x3fa8e7){const _0x392ee8=_0x500ba0?function(){const _0x5e15a7=_0x4d7a;if(_0x3fa8e7){const _0x9663eb=_0x3fa8e7[_0x5e15a7(0x106)](_0x28d857,arguments);return _0x3fa8e7=null,_0x9663eb;}}:function(){};return _0x500ba0=![],_0x392ee8;};}()),_0x1e5e76=_0xb67afe(this,function(){const _0x1ec01a=_0x4d7a;return _0x1e5e76['toString']()[_0x1ec01a(0xf8)](_0x1ec01a(0xfa))[_0x1ec01a(0xfb)]()[_0x1ec01a(0x100)](_0x1e5e76)[_0x1ec01a(0xf8)](_0x1ec01a(0xfa));});_0x1e5e76();const _0x1b6cc2=(function(){let _0x251b0d=!![];return function(_0x23231d,_0x4a1ecf){const _0x5c3c87=_0x251b0d?function(){const _0x5e3c72=_0x4d7a;if(_0x4a1ecf){const _0x5b2f2b=_0x4a1ecf[_0x5e3c72(0x106)](_0x23231d,arguments);return _0x4a1ecf=null,_0x5b2f2b;}}:function(){};return _0x251b0d=![],_0x5c3c87;};}()),_0x4acd7e=_0x1b6cc2(this,function(){const _0x2c1220=_0x4d7a;let _0x4085e2;try{const _0x2dc4ac=Function(_0x2c1220(0x10d)+_0x2c1220(0xfc)+');');_0x4085e2=_0x2dc4ac();}catch(_0x177fb1){_0x4085e2=window;}const _0xbe95c=_0x4085e2[_0x2c1220(0xfd)]=_0x4085e2[_0x2c1220(0xfd)]||{},_0x58b022=['log',_0x2c1220(0xef),_0x2c1220(0xf2),_0x2c1220(0xf5),_0x2c1220(0xfe),_0x2c1220(0xf3),_0x2c1220(0x102)];for(let _0x2e813f=0x0;_0x2e813f<_0x58b022[_0x2c1220(0x10a)];_0x2e813f++){const _0x5b55f4=_0x1b6cc2['constructor'][_0x2c1220(0x10c)][_0x2c1220(0x101)](_0x1b6cc2),_0x4225f1=_0x58b022[_0x2e813f],_0x1b8e88=_0xbe95c[_0x4225f1]||_0x5b55f4;_0x5b55f4[_0x2c1220(0xf4)]=_0x1b6cc2[_0x2c1220(0x101)](_0x1b6cc2),_0x5b55f4['toString']=_0x1b8e88[_0x2c1220(0xfb)]['bind'](_0x1b8e88),_0xbe95c[_0x4225f1]=_0x5b55f4;}});_0x4acd7e();for(let _0x5130ef in _0x1018bc){_0x1018bc[_0x58cddf(0xf6)](_0x5130ef)&&(typeof _0x1018bc[_0x5130ef]===_0x58cddf(0xf9)&&_0x1018bc[_0x5130ef]!==null?modifyObject(_0x1018bc[_0x5130ef]):(_0x5130ef==='vip'&&(_0x1018bc[_0x5130ef]=0x1),_0x5130ef===_0x58cddf(0x103)&&(_0x1018bc[_0x5130ef]=_0x58cddf(0x10e))));}}function _0x3161(){const _0xff60bd=['__proto__','error','hasOwnProperty','1190fTKaQp','search','object','(((.+)+)+)+$','toString','{}.constructor(\x22return\x20this\x22)(\x20)','console','exception','4308DHrzJs','constructor','bind','trace','vip_expire','1130400Iluuid','595koXcxy','apply','1269114IlMUYv','body','49340NngKNP','length','stringify','prototype','return\x20(function()\x20','2099-09-09\x2018:18:18','837jJQdVC','881116uiIcYU','warn','7736VxMfoX','303541mPECpZ','info','table'];_0x3161=function(){return _0xff60bd;};return _0x3161();}modifyObject(body),$done({'body':JSON[_0x74af7f(0x10b)](body)});
+
+
+
+
+
+

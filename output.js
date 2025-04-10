083ac63e3c411a8722bb23dfa6789b3a50f00b5b
Mikephie <131840160+Mikephie@users.noreply.github.com>
2025-04-10 15:28:54 +0800

    Update output.js

--- output.js
+++
@@ -1,8 +0,0 @@
-//Wed Apr 09 2025 21:56:01 GMT+0000 (Coordinated Universal Time)
-//Base:https://github.com/echo094/decode-js
-//Modify:https://github.com/smallfawn/decode_action
-var Mike = JSON.parse($response.body);
-Mike.payload.active = true;
-$done({
-  "body": JSON.stringify(Mike)
-});

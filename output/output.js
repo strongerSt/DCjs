19f43fdef5083b6d0cf3a8fc3302035461b08399
Mikephie <131840160+Mikephie@users.noreply.github.com>
2025-04-12 21:32:58 +0800

    Delete output directory

--- output/output.js
+++
@@ -1,4 +0,0 @@
-//Sat Apr 12 2025 00:51:52 GMT+0000 (Coordinated Universal Time)
-//Base:https://github.com/echo094/decode-js
-//Modify:https://github.com/smallfawn/decode_action
-// Sample input file for testing

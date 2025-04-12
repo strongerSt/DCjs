bb4bdbda17a7a6cf26d5dafc3c062bfb27b2a8a0
action <action@github.com>
2025-04-12 01:26:40 +0000

    Add decoded output file

--- output.js
+++ output.js
@@ -1,8 +1,4 @@
-//Sat Apr 12 2025 00:58:50 GMT+0000 (Coordinated Universal Time)
+//Sat Apr 12 2025 01:26:40 GMT+0000 (Coordinated Universal Time)
 //Base:https://github.com/echo094/decode-js
 //Modify:https://github.com/smallfawn/decode_action
-var Mike = JSON.parse($response.body);
-Mike.payload.active = true;
-$done({
-  "body": JSON.stringify(Mike)
-});
+AAencode解密错误: $response is not defined

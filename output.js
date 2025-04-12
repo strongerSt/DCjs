9395b487586eb6e96785e5605f5be80ed99f088e
action <action@github.com>
2025-04-12 03:35:11 +0000

    Add decoded output file

--- output.js
+++ output.js
@@ -1,5 +1,5 @@
-//Sat Apr 12 2025 03:07:55 GMT+0000 (Coordinated Universal Time)
+//Sat Apr 12 2025 03:35:11 GMT+0000 (Coordinated Universal Time)
 //Base:https://github.com/echo094/decode-js
 //Modify:https://github.com/smallfawn/decode_action
-// AAencode解码结果 (通过字符提取):
-cococo
+// AAencode解码结果 (针对特定模式):
+constru

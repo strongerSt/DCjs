1eb6af88273eaa37699d2ad06d7fc23d2ac74737
action <action@github.com>
2025-04-12 12:55:42 +0000

    Add decoded output file

--- output.js
+++ output.js
@@ -1,10 +1,10 @@
-//Sat Apr 12 2025 12:53:01 GMT+0000 (Coordinated Universal Time)
+//Sat Apr 12 2025 12:55:42 GMT+0000 (Coordinated Universal Time)
 //Base:https://github.com/echo094/decode-js
 //Modify:https://github.com/smallfawn/decode_action
 /**
  * AAencode/颜文字混淆分析报告
  * 
- * 代码总长度: 98993 字符
+ * 代码总长度: 98728 字符
  * 变量定义数量: 12
  * 
  * 混淆类型: JSFuck变种 + 颜文字混淆
@@ -38,13 +38,6 @@
 /*************************************
 
 >「 脚本名称 」         Mix、Mix2解锁Vip
->「 脚本作者 」         M̆̈̆̈ĭ̈̆̈k̆̈̆̈ĕ̈
->「 电报频道 」         https://t.me/TrollStoreKios 
->「 更新时间 」         2024-11-29
->「 注意事项 」         如需引用请注明出处，谢谢合作！
->「 注意事项 」         使用此脚本，会导致AppleStore无法切换账户，解决方法[关闭QX切换账户，或关闭MITM，或删除脚本，或去设置媒体与购买项目处切换ID]
->「 额外说明 」         请勿传播或售卖此脚本
-
 [rewrite_local]
 ^https?:\/\/(cdn-bm|bmall)\.camera360\.com\/api\/(mix\/(getinfo|purchase|recovery)|iap\/check-receipt|mix-asset\/assets) url script-response-body https://raw.githubusercontent.com/Mike-offers/Rewrite/refs/heads/master/QuantumultX/Mix.js
 https:\/\/mix-api\.camera360\.com\/v\d\/operational-positions url reject
